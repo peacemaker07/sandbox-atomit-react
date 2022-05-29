bec0b948d70cce6952e81f31a233311e75a5561b
Yuji Chino <chino@jsl.co.jp>
2022-05-29 15:06:05 +0900

    Initial commit

---
+++ src/components/atoms/organisms/user/UserCard.jsx
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { Card } from "../../card/Card";
+import { UserIconWithName } from "../../molecules/user/userIconWithName";
+
+export const UserCard = (props) => {
+  const { user } = props;
+
+  return (
+    <Card>
+      <UserIconWithName image={user.image} name={user.name} />
+      <SDl>
+        <dt>メール</dt>
+        <dd>{user.mail}</dd>
+        <dt>TEL</dt>
+        <dd>{user.phone}</dd>
+        <dt>会社名</dt>
+        <dd>{user.company.name}</dd>
+        <dt>Web</dt>
+        <dd>{user.website}</dd>
+      </SDl>
+    </Card>
+  );
+};
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left: 32px;
+    padding-bottom: 8px;
+    overflow-wrap: break-word;
+  }
+`;
