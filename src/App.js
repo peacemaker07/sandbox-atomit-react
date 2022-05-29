4668eed63b1494f1965a42ca9953966fa90aa9e5
Yuji Chino <chino@jsl.co.jp>
2022-05-29 15:38:35 +0900

    完成🚀

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import { BrowserRouter } from "react-router-dom";
-import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
-import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
-import { SearchInput } from "./components/atoms/molecules/SearchInput";
-import { UserCard } from "./components/atoms/organisms/user/UserCard";
-import { DefaultLayout } from "./components/templates/DefaultLayout";
-import { HeaderOnly } from "./components/templates/HeaderOnly";
 import "./styles.css";
+import { Router } from "./router/Router";
 
 export default function App() {
-  const user = {
-    name: "chino",
-    image: "https://source.unsplash.com/NE0XGVKTmcA",
-    mail: "test@jsl.co.jp",
-    phone: "00-1111-2222",
-    company: {
-      name: "JSL"
-    },
-    website: "https://jsl.co.jp"
-  };
-
-  return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <PrimaryButton>テスト</PrimaryButton>
-        <SecondaryButton>検索</SecondaryButton>
-        <br />
-        <SearchInput></SearchInput>
-        <UserCard user={user} />
-      </DefaultLayout>
-    </BrowserRouter>
-  );
+  return <Router />;
 }
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,13 +1,46 @@
 import styled from "styled-components";
+import { SearchInput } from "../atoms/molecules/SearchInput";
+import { UserCard } from "../atoms/organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+  return {
+    key: val,
+    name: `chino-${val}`,
+    image: "https://source.unsplash.com/NE0XGVKTmcA",
+    mail: "test@jsl.co.jp",
+    phone: "00-1111-2222",
+    company: {
+      name: "JSL"
+    },
+    website: "https://jsl.co.jp"
+  };
+});
 
 export const Users = () => {
   return (
     <SContainer>
       <h2>ユーザ一覧</h2>
+      <SearchInput />
+      <SUserArea>
+        {users.map((user) => (
+          <UserCard key={users.id} user={user} />
+        ))}
+      </SUserArea>
     </SContainer>
   );
 };
 
 const SContainer = styled.div`
-  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 24px;
+`;
+
+const SUserArea = styled.div`
+  padding-top: 40px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+  grid-gap: 20px;
 `;
